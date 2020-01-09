8fbf3dc52953eaaf018d02a42ca6c53d3dd48c79
Paul Khoziashev <owner-of-all-real@ya.ru>
2020-01-09 23:14:04 +0500

    new select for tasks

--- modules/tasks.js
+++ modules/tasks.js
@@ -59,80 +59,43 @@ var getByDate = (req, res) => {
   let date = req.params.date;
 
   connection.query(
-    //Здесь какая логика. Получаем задачи, который были заведены в указанную дату
-    "select *, " +
-    "  (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time " +
-    "   from task_log tl " +
-    "  where tl.task_id = t.id" +
-    "  and tl.execution_start < tl.execution_end" +
-    "  and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?) execution_time_day," +
-    "  (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time " +
-    "   from task_log tl " +
-    "  where tl.task_id = t.id" +
-    "  and tl.execution_start < tl.execution_end" +
-    "  and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') <= ?) execution_time_to_day" +
-    "  from (" +
     " select t.id," +
-    "  t.user_id, " +
-    "  t.name, " +
-    "  t.name_style, " +
-    "  t.description, " +
-    "  c.name category_name, " +
-    "  t.category_id, " +
-    "  ts.name status_name, " +
-    "  t.status_id" +
-    " from tasks t, " +
-    "  categories c, " +
-    "  task_statuses ts" +
-    " where t.category_id = c.id " +
-    "   and t.status_id = ts.id " +
-    "   and DATE_FORMAT(t.create_date,'%Y-%m-%d') = ? " +
-    "   and t.user_id =? " +
-    //Потом получаем задачи, у которых тип статуса не «завершено» или которые были обновлены в этот день
-    " union  " +
-    " select t.id, " +
-    "  t.user_id, " +
-    "  t.name, " +
-    "  t.name_style, " +
-    "  t.description, " +
-    "  c.name category_name, " +
-    "  t.category_id, " +
-    "  ts.name status_name, " +
-    "  t.status_id" +
-    " from tasks t, " +
-    "  categories c, " +
-    "  task_statuses ts" +
-    " where t.category_id = c.id " +
-    "   and t.status_id = ts.id " +
-    "   and DATE_FORMAT(t.create_date,'%Y-%m-%d') <= ? " +
-    "   and t.user_id = ?" +
-    "   and (ts.type_id !=2 or DATE_FORMAT(t.update_date,'%Y-%m-%d') = ?)  " + //Не завершено
-      //И все задачи, по которым были записи в этот день
-      " union " +
-      " select t.id, " +
       " t.user_id, " +
-      " t.name, " +
+      "  t.name, " +
       " t.name_style, " +
-      " t.description, " +
+      "  t.description, " +
       " c.name category_name, " +
-      " t.category_id, " +
-      " ts.name status_name, " +
-      " t.status_id" +
-      " from tasks t, " +
-      " categories c, " +
-      " task_statuses ts" +
+      "  t.category_id, " +
+      "  ts.name status_name, " +
+      "  t.status_id," +
+      " (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time " +
+      "  from task_log tl " +
+      "  where tl.task_id = t.id" +
+      "     and tl.execution_start < tl.execution_end" +
+      "    and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?) execution_time_day," +
+      "  (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time" +
+      "   from task_log tl " +
+      "  where tl.task_id = t.id" +
+      "    and tl.execution_start < tl.execution_end" +
+      "   and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') <= ?) execution_time_to_day" +
+      " from  tasks t, " +
+      "  categories c, " +
+      "  task_statuses ts" +
       " where t.category_id = c.id " +
       " and t.status_id = ts.id " +
-      " and DATE_FORMAT(t.create_date,'%Y-%m-%d') <= ? " +
-      " and t.user_id = ?" +
-      //Уот здесь как раз чекаем наличие записей
-      " and exists (select * from task_log tl" +
-      "              where tl.task_id = t.id" +
-      "                and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?" +
-      "              limit 1)" +
-      " ) t" +
-      "  order by 1 asc", //Сортируем по ID
-    [date, date, date, user.id, date, user.id, date, date, user.id, date],
+      //Получаем задачи, который были заведены в указанную дату
+      " and (DATE_FORMAT(t.create_date,'%Y-%m-%d') = ? or " +
+      //Или все еще не завершены или были обновлены в указанный день
+      "   (ts.type_id !=2 or DATE_FORMAT(t.update_date,'%Y-%m-%d') = ?) or " +
+      //Или по ним были записи в этот день
+      "  exists (select * " +
+      "            from task_log tl" +
+      "   where tl.task_id = t.id" +
+      "             and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?" +
+      "  limit 1))" +
+      " and t.user_id = 1 " +
+      " order by 1 asc", //Сортируем по ID
+    [date, date, date, date, date, user.id],
 
     //Преобразуем стили в объект
     function(error, results) {
