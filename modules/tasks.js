f5250881c72b212a84bcaad1cd66d6547f304f96
treepoint <owner-of-all-real@ya.ru>
2020-03-01 14:51:48 +0500

    finaly remove style from tasks

--- modules/tasks.js
+++ modules/tasks.js
@@ -12,31 +12,24 @@ var getById = (req, res) => {
 
   connection.query(
     "select " +
-      " t.id, " +
-      " t.user_id, " +
-      " t.name, " +
-      " t.name_style, " +
-      " t.description, " +
-      " c.name category_name, " +
-      " t.category_id," +
-      " ts.name status_name, " +
-      " t.status_id, " +
-      " t.in_archive, " +
-      " t.on_fire, " +
-      " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
-      " null execution_time_day, " +
-      " null execution_time_to_day " +
-      " from tasks t, categories c, task_statuses ts" +
-      " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
+    " t.id, " +
+    " t.user_id, " +
+    " t.name, " +
+    " t.description, " +
+    " c.name category_name, " +
+    " t.category_id," +
+    " ts.name status_name, " +
+    " t.status_id, " +
+    " t.in_archive, " +
+    " t.on_fire, " +
+    " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
+    " null execution_time_day, " +
+    " null execution_time_to_day " +
+    " from tasks t, categories c, task_statuses ts" +
+    " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
     [id, user.id],
-    function(error, results) {
-      let result = results.map(item => {
-        item.name_style = JSON.parse(item.name_style);
-
-        return item;
-      });
-
-      utils.sendResultOrCode(error, utils.arrayToIdObject(result), res, 404);
+    function (error, results) {
+      utils.sendResultOrCode(error, utils.arrayToIdObject(results), res, 404);
     }
   );
 };
@@ -48,58 +41,49 @@ var getByDate = (req, res) => {
 
   connection.query(
     " select t.id," +
-      " t.user_id, " +
-      "  t.name, " +
-      "  t.name_style, " +
-      "  t.description, " +
-      "  c.name category_name, " +
-      "  t.category_id, " +
-      "  ts.name status_name, " +
-      "  t.status_id," +
-      "  t.in_archive," +
-      "  t.on_fire," +
-      "  DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
-      " ? for_date, " +
-      " (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time " +
-      "  from task_log tl " +
-      "  where tl.task_id = t.id" +
-      "     and tl.execution_start < tl.execution_end" +
-      "    and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?) execution_time_day," +
-      "  (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time" +
-      "   from task_log tl " +
-      "  where tl.task_id = t.id" +
-      "    and tl.execution_start < tl.execution_end" +
-      "   and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') <= ?) execution_time_to_day" +
-      " from  tasks t, " +
-      "  categories c, " +
-      "  task_statuses ts" +
-      " where t.category_id = c.id " +
-      " and t.status_id = ts.id " +
-      //Получаем задачи, который были заведены в указанную дату
-      " and (DATE_FORMAT(t.create_date,'%Y-%m-%d') <= ?) " +
-      //Или все еще не завершены или были обновлены в указанный день
-      " and ((ts.type_id !=2 or DATE_FORMAT(t.update_date,'%Y-%m-%d') = ?) or " +
-      //Или по ним были записи в этот день
-      "  exists (select * " +
-      "            from task_log tl" +
-      "   where tl.task_id = t.id" +
-      "             and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?" +
-      "  limit 1))" +
-      //Получаем задачи, дата переноса которых равна или меньшее нужной даты. Ну или пустая
-      " and (DATE_FORMAT(t.moved_date,'%Y-%m-%d') <= ? or t.moved_date is null) " +
-      " and t.user_id = ? " +
-      " order by 1 asc", //Сортируем по ID
+    " t.user_id, " +
+    "  t.name, " +
+    "  t.description, " +
+    "  c.name category_name, " +
+    "  t.category_id, " +
+    "  ts.name status_name, " +
+    "  t.status_id," +
+    "  t.in_archive," +
+    "  t.on_fire," +
+    "  DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
+    " ? for_date, " +
+    " (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time " +
+    "  from task_log tl " +
+    "  where tl.task_id = t.id" +
+    "     and tl.execution_start < tl.execution_end" +
+    "    and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?) execution_time_day," +
+    "  (select SUM(TIMESTAMPDIFF(MINUTE, tl.execution_start, tl.execution_end)) execution_time" +
+    "   from task_log tl " +
+    "  where tl.task_id = t.id" +
+    "    and tl.execution_start < tl.execution_end" +
+    "   and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') <= ?) execution_time_to_day" +
+    " from  tasks t, " +
+    "  categories c, " +
+    "  task_statuses ts" +
+    " where t.category_id = c.id " +
+    " and t.status_id = ts.id " +
+    //Получаем задачи, который были заведены в указанную дату
+    " and (DATE_FORMAT(t.create_date,'%Y-%m-%d') <= ?) " +
+    //Или все еще не завершены или были обновлены в указанный день
+    " and ((ts.type_id !=2 or DATE_FORMAT(t.update_date,'%Y-%m-%d') = ?) or " +
+    //Или по ним были записи в этот день
+    "  exists (select * " +
+    "            from task_log tl" +
+    "   where tl.task_id = t.id" +
+    "             and DATE_FORMAT(tl.execution_start,'%Y-%m-%d') = ?" +
+    "  limit 1))" +
+    //Получаем задачи, дата переноса которых равна или меньшее нужной даты. Ну или пустая
+    " and (DATE_FORMAT(t.moved_date,'%Y-%m-%d') <= ? or t.moved_date is null) " +
+    " and t.user_id = ? " +
+    " order by 1 asc", //Сортируем по ID
     [date, date, date, date, date, date, date, user.id],
-
-    //Преобразуем стили в объект
-    function(error, results) {
-      let result = results.map(item => {
-        item.name_style = JSON.parse(item.name_style);
-
-        return item;
-      });
-
-      utils.sendResultOrCode(error, utils.arrayToIdObject(result), res, 404);
+    function (error, results) {
+      utils.sendResultOrCode(error, utils.arrayToIdObject(results), res, 404);
     }
   );
 };
@@ -112,40 +96,33 @@ var add = (req, res) => {
   let task = {};
   Object.assign(task, { user_id: user.id }, req.body);
 
-  connection.query("insert into tasks set ?", task, function(error, results) {
+  connection.query("insert into tasks set ?", task, function (error, results) {
     //Если добавили — получим этот объект и вернем уже его
     if (typeof results.insertId === "number") {
       connection.query(
         "select " +
-          " t.id, " +
-          " t.user_id, " +
-          " t.name, " +
-          " t.name_style, " +
-          " t.description, " +
-          " c.name category_name, " +
-          " t.category_id," +
-          " ts.name status_name, " +
-          " t.status_id, " +
-          " t.in_archive, " +
-          " t.on_fire, " +
-          " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
-          " ? for_date, " +
-          " null execution_time_day, " +
-          " null execution_time_to_day " +
-          " from tasks t, categories c, task_statuses ts" +
-          " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
+        " t.id, " +
+        " t.user_id, " +
+        " t.name, " +
+        " t.description, " +
+        " c.name category_name, " +
+        " t.category_id," +
+        " ts.name status_name, " +
+        " t.status_id, " +
+        " t.in_archive, " +
+        " t.on_fire, " +
+        " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
+        " ? for_date, " +
+        " null execution_time_day, " +
+        " null execution_time_to_day " +
+        " from tasks t, categories c, task_statuses ts" +
+        " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
         [task.create_date, results.insertId, user.id],
-        function(error, results) {
-          //Преобразуем стили в объект
-          let result = results.map(item => {
-            item.name_style = JSON.parse(item.name_style);
-
-            return item;
-          });
+        function (error, results) {
           //Если получилось — вернем результат или код ошибки
           utils.sendResultOrCode(
             error,
-            utils.arrayToIdObject(result),
+            utils.arrayToIdObject(results),
             res,
             400
           );
@@ -166,22 +143,20 @@ var updateById = (req, res) => {
 
   connection.query(
     "update tasks set " +
-      " category_id=?, " +
-      " status_id=?, " +
-      " name=?, " +
-      " name_style=?, " +
-      " description=?, " +
-      " update_date =?, " +
-      " in_archive =?, " +
-      " on_fire =?, " +
-      " moved_date =? " +
-      "where id=? " +
-      "  and user_id =?",
+    " category_id=?, " +
+    " status_id=?, " +
+    " name=?, " +
+    " description=?, " +
+    " update_date =?, " +
+    " in_archive =?, " +
+    " on_fire =?, " +
+    " moved_date =? " +
+    "where id=? " +
+    "  and user_id =?",
     [
       task.category_id,
       task.status_id,
       task.name,
-      JSON.stringify(task.name_style),
       task.description,
       task.update_date,
       task.in_archive,
@@ -190,39 +165,32 @@ var updateById = (req, res) => {
       task.id,
       user.id
     ],
-    function(error, results) {
+    function (error, results) {
       //Если обновили — получим этот объект и вернем уже его
       if (typeof results.affectedRows === "number") {
         connection.query(
           "select " +
-            " t.id, " +
-            " t.user_id, " +
-            " t.name, " +
-            " t.name_style, " +
-            " t.description, " +
-            " c.name category_name, " +
-            " t.category_id," +
-            " ts.name status_name, " +
-            " t.status_id, " +
-            " t.in_archive, " +
-            " t.on_fire, " +
-            " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
-            " null execution_time_day, " +
-            " null execution_time_to_day " +
-            " from tasks t, categories c, task_statuses ts" +
-            " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
+          " t.id, " +
+          " t.user_id, " +
+          " t.name, " +
+          " t.description, " +
+          " c.name category_name, " +
+          " t.category_id," +
+          " ts.name status_name, " +
+          " t.status_id, " +
+          " t.in_archive, " +
+          " t.on_fire, " +
+          " DATE_FORMAT(t.moved_date,'%Y-%m-%d') moved_date," +
+          " null execution_time_day, " +
+          " null execution_time_to_day " +
+          " from tasks t, categories c, task_statuses ts" +
+          " where t.category_id = c.id and t.status_id = ts.id and t.id=? and t.user_id =?",
           [task.id, user.id],
-          function(error, results) {
-            //Преобразуем стили в объект
-            let result = results.map(item => {
-              item.name_style = JSON.parse(item.name_style);
-
-              return item;
-            });
+          function (error, results) {
             //Если получилось — вернем результат или код ошибки
             utils.sendResultOrCode(
               error,
-              utils.arrayToIdObject(result),
+              utils.arrayToIdObject(results),
               res,
               520
             );
@@ -243,7 +211,7 @@ var deleteById = (req, res) => {
       connection.query(
         "delete from tasks where id=? and user_id=?",
         [id, user.id],
-        function(error, results) {
+        function (error, results) {
           utils.sendResultOrCode(error, results, res, 520);
         }
       );
